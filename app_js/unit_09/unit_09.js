f6101b6f07aa3cb367dc25761c1dd8e57c77ba24
Matvey <luk.matvey@yandex.ru>
2022-12-08 01:42:13 +0300

    unit 9 task 15

--- app_js/unit_09/unit_09.js
+++ app_js/unit_09/unit_09.js
@@ -96,19 +96,16 @@ function f9() {
 let div9 = document.querySelectorAll(".out-9");
 
 for (let i = 0; i < div9.length; i++) {
-  div9[i].onclick 
+  div9[i].onclick = f9;
 }
 
 //  Task 10
 // Усложним предыдущие задачи. С помощью цикла повим на блоки .out-10 событие клик. По клику должна выполняться функция f10. Функция, должна делать toggle класса .bg-orange тому .out-10 на котором кликнули.
 
-let div10 = document.querySelectorAll("out-10");
+let div10 = document.querySelectorAll(".out-10");
 
 function f10() {
-  if (div10.classList.contains("bg-orange")) {
-    this.classList.toggle("div10");
-  }
-  else
+  this.classList.toggle("bg-orange");
 }
 
 for (let i = 0; i < div10.length; i++) {
@@ -120,21 +117,40 @@ for (let i = 0; i < div10.length; i++) {
 //  Task 11
 // Кнопка .b-11, запускает функцию f11. Функция создает через createElement div c текстом 25 и добавляет его через append в .out-11.
 
-function f11() {}
+function f11() {
+  let out11 = document.querySelector(".out-11");
+  let div = document.createElement("div");
+  div.textContent = "25";
+  out11.append(div);
+}
 
 document.querySelector(".b-11").onclick = f11;
 
 //  Task 12
 // Добавьте кнопку .b-12, которая запускает функцию f12. Функция создает через createElement div c текстом 12 и добавляет ему класс bg-12. Созданный div добавляется в out-12.
 
-function f12() {}
+function f12() {
+  let out12 = document.querySelector(".out-12");
+  let div = document.createElement("div");
+  div.textContent = "12";
+  div.classList.add("bg-12");
+
+  out12.append(div);
+}
 
 document.querySelector(".b-12").onclick = f12;
 
 //  Task 13
 // Кнопка .b-13, запускает функцию f13. Функция создает через createElement div c текстом pushMe и добавляет ему класс bg-orange. Также, созданному div добавляется событие onclick, по которому выполняется функция f13_1. Созданный div добавляется в .out-13.
 
-function f13() {}
+function f13() {
+  let out13 = document.querySelector(".out-13");
+  let div = document.createElement("div");
+  div.textContent = "pushMe";
+  div.classList.add("bg-orange");
+  div.onclick = f13_1;
+  out13.append(div);
+}
 
 function f13_1() {
   document.querySelector(".out-13-1").innerHTML += this.innerHTML;
@@ -145,14 +161,26 @@ document.querySelector(".b-13").onclick = f13;
 //  Task 14
 // Кнопка .b-14, запускает функцию f14. Функция создает через createElement div c текстом 14 и добавляет ему класс .bg-orange. Созданный div добавляется в .out-14 с помощью append.
 
-function f14() {}
+function f14() {
+  let out14 = document.querySelector(".out-14");
+  let div = document.createElement("div");
+  div.textContent = "14";
+  div.classList.add("bg-orange");
+  out14.append("div");
+}
 
 document.querySelector(".b-14").onclick = f14;
 
 //  Task 15
 // Кнопкa .b-15, которая запускает функцию f15. Функция создает через createElement div c текстом 15 и добавляет ему класс .bg-orange. Созданный div добавляется перед .out-15 с помощью before.
 
-function f15() {}
+function f15() {
+  let out15 = document.querySelector(".out-15");
+  let div = document.createElement("div");
+  div.textContent = "15";
+  div.classList.add("bg-orange");
+  out15.before("div");
+}
 
 document.querySelector(".b-15").onclick = f15;
 
