de84a51cbdbebbc01e0a2b910d80970f7d5b5bdf
Matvey <luk.matvey@yandex.ru>
2023-03-24 14:31:23 +0300

    Unit 16 ready to start

--- app_js/unit_14/unit_14.js
+++ app_js/unit_14/unit_14.js
@@ -10,27 +10,47 @@ function getWeather() {
       return weather.json();
     })
     .then(showWeather);
+}
+const cities = {
+  3143244: "Oslo",
+  3435907: "Buenos Aires",
+  4183849: "Boston",
+  1701667: "Manila",
+};
 
-  function showWeather(data) {
-    console.log(data);
-    document.querySelector(".package-country").textContent = data.sys.country;
-    document.querySelector(".package-city").textContent = data.name;
-    document.querySelector(".temperature").innerHTML =
-      Math.round(data.main.temp) + "&deg";
-    document.querySelector(".disclaimer").textContent =
-      data.weather[0]["description"];
-    document.querySelector(
-      ".features li"
-    ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`;
-    document.querySelector(".speed").innerHTML =
-      "speed " + data.wind.speed + " m/s";
-    document.querySelector(".humidity").innerHTML =
-      "humidity " + data.main.humidity + " %";
-    document.querySelector(".pressure").innerHTML =
-      "pressure " + data.main.pressure + " kPa";
+function createSelector() {
+  let select = document.createElement("select");
+  select.id = "city";
+
+  for (let key in cities) {
+    let option = document.createElement("option");
+    option.value = key;
+    option.textContent = cities[key];
+    select.append(option);
   }
+  document.querySelector(".select").append(select);
+}
+
+createSelector();
+
+function showWeather(data) {
+  console.log(data);
+  document.querySelector(".package-country").textContent = data.sys.country;
+  document.querySelector(".package-city").textContent = data.name;
+  document.querySelector(".temperature").innerHTML =
+    Math.round(data.main.temp) + "&deg";
+  document.querySelector(".disclaimer").textContent =
+    data.weather[0]["description"];
+  document.querySelector(
+    ".features li"
+  ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`;
+  document.querySelector(".speed").innerHTML =
+    "speed " + data.wind.speed + " m/s";
+  document.querySelector(".humidity").innerHTML =
+    "humidity " + data.main.humidity + " %";
+  document.querySelector(".pressure").innerHTML =
+    "pressure " + data.main.pressure + " kPa";
 }
 
 getWeather();
 document.querySelector("#city").onchange = getWeather;
-// document.querySelector(".button-primary").onclick = getWeather;
